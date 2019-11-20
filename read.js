b7be23c26f8959ed8be43e3f11f429cf63e9511f
Stanislas Girard <stanislasgirard@MacBook-Pro-de-Stanislas.local>
2019-11-20 09:10:10 +0100

    memory usage

--- read.js
+++ read.js
@@ -71,6 +71,10 @@ async function processArray(files) {
                     console.log(read)
                 }
             }
+            const used = process.memoryUsage();
+            for (let key in used) {
+            console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+            }
             
             return result
             
